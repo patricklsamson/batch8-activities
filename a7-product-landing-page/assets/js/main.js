1c5a42a8ff498532d824431a321969223c2cbe8f
Slnc Digital Works <79042173+slncdworks@users.noreply.github.com>
2021-04-09 19:53:47 +0800

    added js

---
+++ a7-product-landing-page/assets/js/main.js
@@ -0,0 +1,55 @@
+docReady(function () {
+  matchHeights(".mh");
+  matchHeights(".mh2");
+  matchHeights(".mh3");
+
+  for (var i = 0; i < $a(".nav-link").length; i++) {
+    addEvent($a(".nav-link")[i], "click", function () {
+      $$("nav-toggle").checked = false;
+    });
+  }
+
+  var carousel = new caro({
+    autoplay: {
+      on: true,
+      interval: 5000,
+    },
+  });
+
+  addEvent($$("signup-btn"), "click", function () {
+    tClass(document.body, "modal-open");
+    rClass(document.body, "animate");
+    $$("name").value = "";
+    $$("email").value = "";
+    $$("password").value = "";
+    $$("confirm-pw").value = "";
+    $$("confirm-msg").innerHTML = "";
+  });
+
+  addEvent($$("login-btn"), "click", function () {
+    tClass(document.body, "modal-open");
+    rClass(document.body, "animate");
+    $$("log-email").value = "";
+    $$("log-password").value = "";
+  });
+
+  addEvent($$("password"), "keyup", function () {
+    if ($$("confirm-pw").value == $$("password").value) {
+      $$("submit").disabled = false;
+    } else {
+      $$("submit").disable = true;
+    }
+  });
+
+  addEvent($$("confirm-pw"), "keyup", function () {
+    if ($$("confirm-pw").value == $$("password").value) {
+      $$("submit").disabled = false;
+      $$("confirm-msg").style.color = "green";
+      $$("confirm-msg").innerHTML = " MATCH";
+    } else {
+      $$("submit").disable = true;
+      $$("confirm-msg").style.color = "red";
+      $$("confirm-msg").innerHTML = " NOT MATCH";
+    }
+  });
+});
