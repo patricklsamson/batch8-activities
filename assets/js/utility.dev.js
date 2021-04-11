07a027dc6f730dc2a420cef69ff723cf88666879
Slnc Digital Works <79042173+slncdworks@users.noreply.github.com>
2021-04-12 00:37:16 +0800

    minified js

--- assets/js/utility.dev.js
+++ assets/js/utility.dev.js
@@ -380,19 +380,17 @@ caro = function () {
       });
     }
 
-    if (self.def.autoplay.on) {
+    if (self.def.autoplay.on && !hClass(self.def.target, "isAnimating")) {
       setInterval(function () {
-        if (!hClass(self.def.target, "isAnimating")) {
-          if (self.curSlide == self.totalSlides) {
-            self.curSlide = 0;
-            self.sliderInner.style.left = -self.curSlide * self.slideW + "px";
-          }
-
-          self.curSlide++;
-          setTimeout(function () {
-            self.gotoSlide();
-          }, 20);
+        if (self.curSlide == self.totalSlides) {
+          self.curSlide = 0;
+          self.sliderInner.style.left = -self.curSlide * self.slideW + "px";
         }
+
+        self.curSlide++;
+        setTimeout(function () {
+          self.gotoSlide();
+        }, 20);
       }, self.def.autoplay.interval);
     }
   };
