39a0a446336e51e60c10ddeaa860cae1e5039b6f
Slnc Digital Works <79042173+slncdworks@users.noreply.github.com>
2021-04-12 16:33:28 +0800

    updated js

--- a7-product-landing-page/assets/js/main.js
+++ a7-product-landing-page/assets/js/main.js
@@ -20,6 +20,14 @@ docReady(function () {
       rClass(document.body, "animate");
       $$("log-email").value = "";
       $$("log-password").value = "";
+
+      if ($$("reveal").checked) {
+        $$("reveal").checked = false;
+      }
+
+      if ($$("remember").checked) {
+        $$("remember").checked = false;
+      }
     });
 
     addEvent($$("password"), "keyup", function () {
