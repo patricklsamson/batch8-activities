31304388fb60d3d6d0e0d6fdf66b63f945e0a458
Slnc Digital Works <79042173+slncdworks@users.noreply.github.com>
2021-04-23 11:26:04 +0800

    updated js

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -26,6 +26,10 @@ function qsa(element) {
   return document.querySelectorAll(element);
 }
 
+function create_el(element) {
+  return document.createElement(element);
+}
+
 function add_event(element, e, fn) {
   if (element.addEventListener) {
     element.addEventListener(e, fn, false);
