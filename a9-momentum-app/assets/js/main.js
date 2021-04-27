f1e0eda6b0baec23714db974ecc5c088f81e50a0
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-04-27 22:07:08 +0800

    updated js

--- a9-momentum-app/assets/js/main.js
+++ a9-momentum-app/assets/js/main.js
@@ -15,7 +15,7 @@ doc_ready(() => {
     quotes = ["Every morning is a beautiful morning.", "Shine like the afternoon sun and let people be inspired for all the great things you do.", "Evenings are life's way of saying that you are closer to your dreams."],
     quotesCounter = -1;
 
-  let clock = () => {
+  const clock = () => {
     if (today.getHours() < 12) {
       id("greetings").innerHTML = "Morning";
     } else if (today.getHours() >= 12 && today.getHours() < 18) {
@@ -74,28 +74,33 @@ doc_ready(() => {
     }
   });
 
+  const modalOpen = () => {
+    toggle_class(id("modal"), "show");
+    toggle_class(document.body, "modal-open");
+  };
+
   add_event(id("name"), "keypress", (e) => {
     if ((e.which || e.keyCode) == 13 && id("name").value.length != 0) {
-      toggle_class(id("modal"), "show");
-      toggle_class(document.body, "modal-open");
+      modalOpen();
     }
   });
 
   add_event(id("submit"), "click", () => {
-    toggle_class(id("modal"), "show");
-    toggle_class(document.body, "modal-open");
+    modalOpen();
   });
 
   add_event(id("change"), "click", () => {
-    toggle_class(id("modal"), "show");
-    toggle_class(document.body, "modal-open");
+    modalOpen();
 
     setTimeout(() => {
       id("name").focus();
     }, 500);
   });
 
-  let addedToDo = () => {
+  const addedToDo = () => {
+    todo.push(id("add-to-do").value.substring(0, 1).toUpperCase() + id("add-to-do").value.substring(1).toLowerCase());
+    id("add-to-do").value = "";
+
     const element = create_el("li");
 
     id("to-do-list").appendChild(element);
@@ -106,23 +111,19 @@ doc_ready(() => {
     });
   };
 
-  add_event(id("add-to-do"), "keypress", (e) => {
+  add_events(id("add-to-do"), "keypress", (e) => {
     if ((e.which || e.keyCode) == 13 && id("add-to-do").value.length != 0) {
-      todo.push(id("add-to-do").value.substring(0, 1).toUpperCase() + id("add-to-do").value.substring(1).toLowerCase());
-      id("add-to-do").value = "";
       addedToDo();
     }
   });
 
   add_event(id("add-to-do-btn"), "click", () => {
     if (id("add-to-do").value.length != 0) {
-      todo.push(id("add-to-do").value.substring(0, 1).toUpperCase() + id("add-to-do").value.substring(1).toLowerCase());
-      id("add-to-do").value = "";
       addedToDo();
     }
   });
 
-  let showQuotes = () => {
+  const showQuotes = () => {
     quotesCounter++;
 
     if (quotesCounter == quotes.length) {
@@ -135,7 +136,7 @@ doc_ready(() => {
   showQuotes();
   setInterval(showQuotes, 5000);
 
-  let listQuotes = () => {
+  const listQuotes = () => {
     for (i = 0; i < quotes.length; i++) {
       const element = create_el("p");
 
@@ -147,7 +148,7 @@ doc_ready(() => {
 
   listQuotes();
 
-  let addedQuote = () => {
+  const addedQuote = () => {
     const element = create_el("p");
 
     add_class(element, "mb-05");
