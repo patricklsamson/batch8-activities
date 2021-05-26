d47d02d6cc6b851f31edc2d6f48b5a57a720d4eb
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-05-27 02:42:07 +0800

    updated js

--- a10-bank-app/assets/js/main.js
+++ a10-bank-app/assets/js/main.js
@@ -38,7 +38,7 @@ doc_ready(() => {
       this.transactionHistory = [];
       this.userTransactionHistory = [];
       this.expenseItems = [];
-      this.friends = [];
+      this.connections = [];
     }
   }
 
@@ -162,6 +162,9 @@ doc_ready(() => {
         toggle_class(id("modal"), "hide");
         add_class(id("expense-wrap"), "hide");
         add_class(id("connections-wrap"), "hide");
+        id("withdraw-account").removeAttribute("value");
+        id("deposit-account").removeAttribute("value");
+        id("sender-account").removeAttribute("value");
 
         add_event(id("admin-settings-form"), "submit", (e) => {
           e.preventDefault();
@@ -209,12 +212,19 @@ doc_ready(() => {
 
           return false;
         });
-      } else if (users[usernameCheck] && users[passwordCheck]) {
+      } else if (
+        users[usernameCheck] &&
+        users[passwordCheck] &&
+        usernameCheck == passwordCheck
+      ) {
         for (i = 0; i < users.length; i++) {
           if (users[i].username == username && users[i].password == password) {
             // NEEDED FOR BETTER TRANSITION TIMING WHEN SHOWING WINDOWS
             setTimeout(() => {
               toggle_class(id("modal"), "hide");
+              add_class(id("withdraw-form"), "hide");
+              add_class(id("deposit-form"), "hide");
+              add_class(id("send-form"), "hide");
             }, 250);
 
             add_class(id("settings-modal-inner"), "user");
@@ -237,6 +247,16 @@ doc_ready(() => {
           id("owner-acc-num").innerHTML.split(" ").join("")
         );
 
+        id(
+          "profile-name"
+        ).innerHTML = `${users[usernameCheck].firstName} ${users[usernameCheck].middleName} ${users[usernameCheck].lastName}`;
+
+        id("profile-username").innerHTML = users[usernameCheck].username;
+        id("profile-email").innerHTML = users[usernameCheck].email;
+        add_att(id("withdraw-account"), "value", id("owner-acc-num").innerHTML);
+        add_att(id("deposit-account"), "value", id("owner-acc-num").innerHTML);
+        add_att(id("sender-account"), "value", id("owner-acc-num").innerHTML);
+
         add_event(id("change-email-form"), "submit", (e) => {
           e.preventDefault();
 
@@ -256,7 +276,7 @@ doc_ready(() => {
           e.preventDefault();
 
           for (i = 0; i < users.length; i++) {
-            if (users[i].username == username) {
+            if (users[i].username == id("change-username").value) {
               alert("Username already used!");
               return;
             }
@@ -264,10 +284,6 @@ doc_ready(() => {
 
           if (id("change-username").value.length < 5) {
             alert("Username cannot be less than 5 characters!");
-          } else if (
-            users[usernameCheck].username == id("change-username").value
-          ) {
-            alert("Username already used!");
           } else {
             users[usernameCheck].username = id("change-username").value;
             alert("Change username successful!");
@@ -322,8 +338,10 @@ doc_ready(() => {
 
           return false;
         });
+      } else if (!users[usernameCheck]) {
+        alert("User does not exist!");
       } else {
-        alert("User not found!");
+        alert("Username and password do not match!");
       }
 
       setTimeout(() => {
@@ -362,7 +380,13 @@ doc_ready(() => {
         passwordCheck = users.findIndex((index) => index.password == password),
         emailCheck = users.findIndex((userIndex) => userIndex.email == email);
 
-      if (users[accountNumberCheck].signedUp == false) {
+      if (users[accountNumberCheck].signedUp) {
+        remove_class(id("match-msg"), "fa-check");
+        remove_class(id("match-msg"), "fa-times");
+        alert("You have already signed up!");
+        id("signup-form").reset();
+        return;
+      } else {
         for (i = 0; i < users.length; i++) {
           if (users[i].username == username) {
             alert("Username already used!");
@@ -443,11 +467,117 @@ doc_ready(() => {
          * BY OVERRIDING AND SETTING IT AGAIN, AND ALSO STRINGIFY IT AGAIN TOO
          */
         localStorage.setItem("users", JSON.stringify(users));
+      }
+    }
+
+    static add_connections(user, name, connection) {
+      const users = FnHandler.userStorage();
+
+      let ownerCheck = users.findIndex((index) => index.accountNumber == user),
+        accountNumberCheck = users.findIndex(
+          (index) => index.accountNumber == connection
+        );
+
+      for (i = 0; i < users[ownerCheck].connections.length; i++) {
+        if (users[ownerCheck].connections[j].accountNumber == connection) {
+          alert("User already exists!");
+          return;
+        }
+      }
+
+      if (
+        users[accountNumberCheck] == null ||
+        users[accountNumberCheck] == ""
+      ) {
+        alert("User not found");
+      } else if (user == connection) {
+        alert("Cannot add own account number!");
       } else {
-        remove_class(id("match-msg"), "fa-check");
-        remove_class(id("match-msg"), "fa-times");
-        alert("You have already signed up!");
-        id("signup-form").reset();
+        users[ownerCheck].connections.push({
+          name: name,
+          accountNumber: connection,
+        });
+
+        remove_class(id("connections-form"), "show");
+        id("connections-form").reset();
+        localStorage.setItem("users", JSON.stringify(users));
+      }
+    }
+
+    static list_connections(user) {
+      const users = FnHandler.userStorage();
+
+      let ownerCheck = users.findIndex((index) => index.accountNumber == user);
+
+      id("connections-table").innerHTML = "";
+
+      for (i = 0; i < users[ownerCheck].connections.length; i++) {
+        let tableRow = create_el("tr"),
+          nameTd = create_el("td"),
+          accNumTd = create_el("td"),
+          editTd = create_el("td"),
+          deleteTd = create_el("td");
+
+        nameTd.innerHTML = users[ownerCheck].connections[i].name;
+        tableRow.appendChild(nameTd);
+
+        accNumTd.innerHTML = num_space(
+          users[ownerCheck].connections[i].accountNumber
+        );
+
+        FnHandler.click_copy(accNumTd);
+        tableRow.appendChild(accNumTd);
+
+        editTd.innerHTML = `<i id="${i}" class="fas fa-edit"></i>`;
+
+        add_event(editTd.querySelector("i"), "click", function () {
+          if (!has_class(id("connections-form"), "show")) {
+            add_class(id("connections-form"), "show");
+          }
+
+          id("connections-name").value =
+            users[ownerCheck].connections[this.id].name;
+
+          id("connections-account-num").value =
+            users[ownerCheck].connections[this.id].accountNumber;
+
+          users[ownerCheck].connections.splice(this.id, 1);
+          localStorage.setItem("users", JSON.stringify(users));
+
+          return FnHandler.list_connections(
+            id("owner-acc-num").innerHTML.split(" ").join("")
+          );
+        });
+
+        tableRow.appendChild(editTd);
+
+        deleteTd.innerHTML = `<i id="${i}" class="fas fa-minus-circle"></i>`;
+
+        add_event(deleteTd.querySelector("i"), "click", function () {
+          let deletePrompt = prompt(
+              'Are you sure to delete this connection?\nType "Y" for yes and "N" for no.',
+              "N"
+            ),
+            deleteAnswer =
+              deletePrompt != null
+                ? deletePrompt.toLowerCase()
+                : console.clear();
+
+          if (deleteAnswer == "y") {
+            users[ownerCheck].connections.splice(this.id, 1);
+            localStorage.setItem("users", JSON.stringify(users));
+
+            return FnHandler.list_connections(
+              id("owner-acc-num").innerHTML.split(" ").join("")
+            );
+          } else {
+            return;
+          }
+        });
+
+        tableRow.appendChild(deleteTd);
+
+        id("connections-table").appendChild(tableRow);
       }
     }
 
@@ -519,9 +649,8 @@ doc_ready(() => {
         );
 
         alert(`Withdrawal transaction has been successful!`);
+        localStorage.setItem("users", JSON.stringify(users));
       }
-
-      localStorage.setItem("users", JSON.stringify(users));
     }
 
     static deposit(user, amount) {
@@ -553,15 +682,14 @@ doc_ready(() => {
         );
 
         users[userCheck].userTransactionHistory.unshift(
-          `<em>${FnHandler.time_stamp()}</em> : Deposit transaction amounting to <strong>₱${amount}</strong> into your account has been successful. From a previous account balance of <strong>₱${initialBal}</strong>, your remaining account balance is now <strong>₱${
+          `<em>${FnHandler.time_stamp()}</em> : Deposit transaction amounting to <strong>₱${amount}</strong> into your account has been successful. From a previous account balance of <strong>₱${initialBal}</strong>, your account balance is now <strong>₱${
             users[userCheck].balance
           }</strong>.`
         );
 
         alert(`Deposit transaction account has been successful!`);
+        localStorage.setItem("users", JSON.stringify(users));
       }
-
-      localStorage.setItem("users", JSON.stringify(users));
     }
 
     static send(from_user, to_user, amount) {
@@ -653,9 +781,8 @@ doc_ready(() => {
         );
 
         alert(`Money transfer has been successful!`);
+        localStorage.setItem("users", JSON.stringify(users));
       }
-
-      localStorage.setItem("users", JSON.stringify(users));
     }
 
     static get_balance(user, parentEl) {
@@ -697,12 +824,22 @@ doc_ready(() => {
         accNumTd.innerHTML = num_space(users[i].accountNumber);
         tableRow.appendChild(accNumTd);
         FnHandler.click_copy(accNumTd);
+
         accTdSpan.innerHTML = `${users[i].firstName} ${users[i].middleName} ${users[i].lastName}`;
+
+        add_event(accTdSpan, "click", () => {
+          add_class(historyModal, "show");
+        });
+
         add_class(historyModalClose, "far");
         add_class(historyModalClose, "fa-times-circle");
         add_class(historyModalClose, "fa-2x");
         add_class(historyUl, "xbul");
         add_class(historyUl, "wrap-scroll");
+
+        add_event(historyModalClose, "click", () => {
+          remove_class(historyModal, "show");
+        });
 
         // INDICATION WHEN NO OTHER TRANSACTIONS ARE MADE YET ASIDE FROM OPENING THE ACCOUNT
         if (users[i].transactionHistory.length == 1) {
@@ -748,14 +885,6 @@ doc_ready(() => {
         accTd.appendChild(accTdSpan);
         accTd.appendChild(historyModal);
         tableRow.appendChild(accTd);
-
-        add_event(accTdSpan, "click", () => {
-          add_class(historyModal, "show");
-        });
-
-        add_event(historyModalClose, "click", () => {
-          remove_class(historyModal, "show");
-        });
 
         accTypeTd.innerHTML = users[i].accountType;
         tableRow.appendChild(accTypeTd);
@@ -918,7 +1047,6 @@ doc_ready(() => {
   FnHandler.num_only();
   FnHandler.type_comma();
   FnHandler.dec_addZero();
-  FnHandler.click_copy(id("owner-acc-num"));
 
   FnHandler.password_match(
     id("signup-password"),
@@ -1157,6 +1285,10 @@ doc_ready(() => {
       id("login-password").value
     );
 
+    FnHandler.list_connections(
+      id("owner-acc-num").innerHTML.split(" ").join("")
+    );
+
     // OLD CODE
     // if (users[usernameCheck] && users[passwordCheck]) {
     //   // TO CONTROL WHICH WINDOW WILL APPEAR FOR THE ADMIN AND THE REGULAR USERS
@@ -1199,7 +1331,11 @@ doc_ready(() => {
       remove_class(id("accounts-wrap"), "hide");
       remove_class(id("expense-wrap"), "hide");
       remove_class(id("connections-wrap"), "hide");
+      remove_class(id("connections-form"), "show");
       remove_class(id("add-newaccount-wrap"), "hide");
+      remove_class(id("withdraw-form"), "hide");
+      remove_class(id("deposit-form"), "hide");
+      remove_class(id("send-form"), "hide");
     }, 500);
 
     remove_class(id("settings-modal-inner"), "user");
@@ -1262,9 +1398,33 @@ doc_ready(() => {
     remove_class(id("owner-transaction-modal"), "show");
   });
 
+  add_event(id("owner"), "click", () => {
+    add_class(id("profile-modal"), "show");
+  });
+
+  add_event(id("close-profile-btn"), "click", () => {
+    remove_class(id("profile-modal"), "show");
+  });
+
   add_event(id("add-connections-btn"), "click", () => {
     toggle_class(id("connections-form"), "show");
     id("connections-form").reset();
+  });
+
+  add_event(id("connections-form"), "submit", (e) => {
+    e.preventDefault();
+
+    FnHandler.add_connections(
+      id("owner-acc-num").innerHTML.split(" ").join(""),
+      id("connections-name").value.toUpperCase(),
+      id("connections-account-num").value.split(" ").join("")
+    );
+
+    FnHandler.list_connections(
+      id("owner-acc-num").innerHTML.split(" ").join("")
+    );
+
+    return false;
   });
 
   add_event(id("withdraw-form"), "submit", (e) => {
@@ -1284,9 +1444,11 @@ doc_ready(() => {
     );
 
     FnHandler.list_users();
+
     FnHandler.individual_history(
       id("owner-acc-num").innerHTML.split(" ").join("")
     );
+
     id("withdraw-form").reset();
     return false;
   });
@@ -1304,9 +1466,11 @@ doc_ready(() => {
     );
 
     FnHandler.list_users();
+
     FnHandler.individual_history(
       id("owner-acc-num").innerHTML.split(" ").join("")
     );
+
     id("deposit-form").reset();
     return false;
   });
@@ -1325,9 +1489,11 @@ doc_ready(() => {
     );
 
     FnHandler.list_users();
+
     FnHandler.individual_history(
       id("owner-acc-num").innerHTML.split(" ").join("")
     );
+
     id("send-form").reset();
     return false;
   });
