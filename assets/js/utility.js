056bece36386a74746833c228ab8d52415853335
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-05-27 13:58:17 +0800

    updated js

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -40,6 +40,10 @@ function inner(str) {
     .replace(/\//g, "&#x2F;");
 }
 
+function trim(str) {
+  return str.replace(/\s+/g, " ").trim();
+}
+
 function add_event(element, e, fn) {
   if (element.addEventListener) {
     element.addEventListener(e, fn, false);
