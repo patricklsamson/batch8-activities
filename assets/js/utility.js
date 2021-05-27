5feea37a7e271cfae8ea098b892b0716d9267f71
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-05-28 02:04:41 +0800

    updated js

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -198,6 +198,42 @@ function rand(number) {
   return Math.floor(Math.random() * number);
 }
 
+function localStorage_space() {
+  var data = "";
+
+  console.log("Current local storage: ");
+
+  for (var key in window.localStorage) {
+    if (window.localStorage.hasOwnProperty(key)) {
+      data += window.localStorage[key];
+
+      console.log(
+        key +
+          " = " +
+          ((window.localStorage[key].length * 16) / (8 * 1024)).toFixed(2) +
+          " KB"
+      );
+    }
+  }
+
+  console.log(
+    data
+      ? "\n" +
+          "Total space used: " +
+          ((data.length * 16) / (8 * 1024)).toFixed(2) +
+          " KB"
+      : "Empty (0 KB)"
+  );
+
+  console.log(
+    data
+      ? "Approx. space remaining: " +
+          (5120 - ((data.length * 16) / (8 * 1024)).toFixed(2)) +
+          " KB"
+      : "5 MB"
+  );
+}
+
 function dataset(element, value) {
   if (element.dataset) {
     return element.dataset.value;
