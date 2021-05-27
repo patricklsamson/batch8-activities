8d218244792c83ca2604bdad312fe5e5e9d3f80b
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-05-27 13:58:25 +0800

    minified js

--- assets/js/utility.dev.js
+++ assets/js/utility.dev.js
@@ -36,6 +36,10 @@ function inner(str) {
   return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\"/g, "&quot;").replace(/\'/g, "&#39;").replace(/\//g, "&#x2F;");
 }
 
+function trim(str) {
+  return str.replace(/\s+/g, " ").trim();
+}
+
 function add_event(element, e, fn) {
   if (element.addEventListener) {
     element.addEventListener(e, fn, false);
