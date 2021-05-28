a19c934cba8a2d6bdc07532a363455459bb6ed66
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-05-28 16:16:58 +0800

    updated js

--- a10-bank-app/assets/js/main.js
+++ a10-bank-app/assets/js/main.js
@@ -1795,20 +1795,6 @@ doc_ready(() => {
     return false;
   });
 
-  const remove_filter = () => {
-    for (i = 0; i < id("acc-table").querySelectorAll("tr").length; i++) {
-      remove_class(
-        id("acc-table").querySelectorAll("tr[class*='savings']")[i],
-        "hide"
-      );
-
-      remove_class(
-        id("acc-table").querySelectorAll("tr[class*='checking']")[i],
-        "hide"
-      );
-    }
-  };
-
   add_event(id("log-out-btn"), "click", (e) => {
     e.preventDefault();
     toggle_class(id("modal"), "hide");
@@ -1818,7 +1804,12 @@ doc_ready(() => {
       remove_class(id("accounts-wrap"), "hide");
       id("filter-all").checked = true;
       id("search-name").value = "";
-      remove_filter();
+
+      for (i = 0; i < id("acc-table").querySelectorAll("tr").length; i++) {
+        remove_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+        remove_class(id("acc-table").querySelectorAll("tr")[i], "search-hide");
+      }
+
       remove_class(id("expense-wrap"), "hide");
       remove_class(id("connections-wrap"), "hide");
       remove_class(id("connections-form"), "show");
@@ -1870,33 +1861,29 @@ doc_ready(() => {
     FnHandler.reset();
   });
 
-  add_event(id("filter-all"), "click", remove_filter);
+  add_event(id("filter-all"), "click", () => {
+    for (i = 0; i < id("acc-table").querySelectorAll("tr").length; i++) {
+      remove_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+    }
+  });
 
   add_event(id("filter-savings"), "click", () => {
     for (i = 0; i < id("acc-table").querySelectorAll("tr").length; i++) {
-      remove_class(
-        id("acc-table").querySelectorAll("tr[class*='savings']")[i],
-        "hide"
-      );
-
-      add_class(
-        id("acc-table").querySelectorAll("tr[class*='checking']")[i],
-        "hide"
-      );
+      if (has_class(id("acc-table").querySelectorAll("tr")[i], "checking")) {
+        add_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+      } else {
+        remove_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+      }
     }
   });
 
   add_event(id("filter-checking"), "click", () => {
     for (i = 0; i < id("acc-table").querySelectorAll("tr").length; i++) {
-      add_class(
-        id("acc-table").querySelectorAll("tr[class*='savings']")[i],
-        "hide"
-      );
-
-      remove_class(
-        id("acc-table").querySelectorAll("tr[class*='checking']")[i],
-        "hide"
-      );
+      if (has_class(id("acc-table").querySelectorAll("tr")[i], "savings")) {
+        add_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+      } else {
+        remove_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+      }
     }
   });
 
@@ -1916,9 +1903,12 @@ doc_ready(() => {
             .innerHTML.toUpperCase()
             .indexOf(id("search-name").value.toUpperCase()) > -1
         ) {
-          remove_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+          remove_class(
+            id("acc-table").querySelectorAll("tr")[i],
+            "search-hide"
+          );
         } else {
-          add_class(id("acc-table").querySelectorAll("tr")[i], "hide");
+          add_class(id("acc-table").querySelectorAll("tr")[i], "search-hide");
         }
       }
     }
