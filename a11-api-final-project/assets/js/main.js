b9a63b0117dca185a7cb30d944c89c476d6c19d9
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-11 18:08:31 +0800

    updated js

--- a11-api-final-project/assets/js/main.js
+++ a11-api-final-project/assets/js/main.js
@@ -18,21 +18,58 @@ doc_ready(() => {
     loadQuote();
     remove_class(id("header"), "hide");
     add_class(document.body, "modal-open");
+
+    setTimeout(() => {
+      id("advice-ul").innerHTML = "Search for an advice or roll the dice!";
+      id("bored-activity").innerHTML = "Roll the dice!";
+    }, 500);
   });
 
-  add_event(id("advice-btn"), "click", () => {
-    remove_class(id("advice-btn"), "inactive");
-    add_class(id("bored-btn"), "inactive");
-    add_class(id("modal-wrap"), "advice");
-    remove_class(id("modal-wrap"), "bored");
-  });
+  for (i = 0; i < qsel_all(".tab-btn").length; i++) {
+    add_event(qsel_all(".tab-btn")[i], "click", function () {
+      for (j = 0; j < qsel_all(".tab-btn").length; j++) {
+        add_class(qsel_all(".tab-btn")[j], "inactive");
 
-  add_event(id("bored-btn"), "click", () => {
-    remove_class(id("bored-btn"), "inactive");
-    add_class(id("advice-btn"), "inactive");
-    add_class(id("modal-wrap"), "bored");
-    remove_class(id("modal-wrap"), "advice");
-  });
+        remove_class(
+          id("modal-wrap"),
+          qsel_all(".tab-btn")[j].id.split("-")[0]
+        );
+
+        if (has_class(this, "inactive")) {
+          remove_class(this, "inactive");
+          add_class(id("modal-wrap"), this.id.split("-")[0]);
+        }
+      }
+    });
+  }
+
+  // OLD CODE
+  // add_event(id("advice-btn"), "click", () => {
+  //   remove_class(id("advice-btn"), "inactive");
+  //   add_class(id("bored-btn"), "inactive");
+  //   add_class(id("sos-btn"), "inactive");
+  //   add_class(id("modal-wrap"), "advice");
+  //   remove_class(id("modal-wrap"), "bored");
+  //   remove_class(id("modal-wrap"), "sos");
+  // });
+
+  // add_event(id("bored-btn"), "click", () => {
+  //   remove_class(id("bored-btn"), "inactive");
+  //   add_class(id("advice-btn"), "inactive");
+  //   add_class(id("sos-btn"), "inactive");
+  //   add_class(id("modal-wrap"), "bored");
+  //   remove_class(id("modal-wrap"), "advice");
+  //   remove_class(id("modal-wrap"), "sos");
+  // });
+
+  // add_event(id("sos-btn"), "click", () => {
+  //   remove_class(id("sos-btn"), "inactive");
+  //   add_class(id("advice-btn"), "inactive");
+  //   add_class(id("bored-btn"), "inactive");
+  //   add_class(id("modal-wrap"), "sos");
+  //   remove_class(id("modal-wrap"), "advice");
+  //   remove_class(id("modal-wrap"), "bored");
+  // });
 
   add_event(id("advice-form"), "submit", (e) => {
     e.preventDefault();
@@ -62,4 +99,53 @@ doc_ready(() => {
         .then((data) => (id("bored-activity").innerHTML = data.activity));
     }, 500);
   });
+
+  fetch("https://restcountries.eu/rest/v2/all?fields=name;callingCodes")
+    .then((response) => response.json())
+    .then((data) => {
+      for (i = 0; i < data.length; i++) {
+        let div = create_el("div"),
+          nameLower = data[i].name.split(" ").join("").toLowerCase();
+
+        add_class(div, "option");
+        div.innerHTML = `<input type="radio" name="country-code" id="${nameLower}" /><label for="${nameLower}">(+${data[i].callingCodes[0]}) ${data[i].name}</label>`;
+
+        setTimeout(() => {
+          qsel(".options-container").appendChild(div);
+        }, 1000);
+      }
+    });
+
+  setTimeout(() => {
+    search_sel();
+  }, 2500);
+
+  add_event(id("sos-form"), "submit", (e) => {
+    e.preventDefault();
+
+    if (qsel(".selected").innerHTML != "Code") {
+      sendLocation(
+        inner(trim(id("sos-name").value.toUpperCase())),
+        qsel(".selected").innerHTML.split(" ")[0] + id("sos-number").value,
+        id("sos-message").value
+      );
+    }
+
+    return false;
+  });
+
+  add_event(id("sos-name"), "keypress", (e) => {
+    if (
+      (e.which == 13 || e.keyCode == 13) &&
+      qsel(".selected").innerHTML != "Code"
+    ) {
+      sendLocation(
+        inner(trim(id("sos-name").value.toUpperCase())),
+        qsel(".selected").innerHTML.split(" ")[0] + id("sos-number").value,
+        id("sos-message").value
+      );
+    }
+  });
+
+  add_att(id("sos-number"), "onkeypress", "return num_only(event)");
 });
