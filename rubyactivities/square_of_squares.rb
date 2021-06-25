b241648b59491891c47189097d200d0043eaff4a
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-25 20:49:25 +0800

    added square of squares

---
+++ rubyactivities/square_of_squares.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def square?(num)
+  if num.negative?
+
+    puts false
+
+  else
+
+    puts (num.to_i**0.5 % 1).zero?
+
+  end
+end
+
+numbers = [-1, 0, 3, 4, 25, 26]
+
+numbers.each { |n| puts square?(n) }
