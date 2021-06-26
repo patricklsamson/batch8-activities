cfce141cf76c8cecfc849da74ad84ce5f5b15847
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-26 12:08:24 +0800

    updated age

--- a1-ruby/age.rb
+++ a1-ruby/age.rb
@@ -1,25 +1,18 @@
 # frozen_string_literal: true
 
 # 4A
-
 puts 'How old are you?'
-
 age = gets.chomp.to_i
 
 # OLD CODE
-
 # years = [10, 20, 30, 40]
 
 # years.each do |y|
-
 #   puts "In #{y} years you will be:"
-
 #   puts age + y
-
 # end
 
 (1..4).each do |i|
   puts "In #{i * 10} years you will be:"
-
   puts i * 10 + age
 end
