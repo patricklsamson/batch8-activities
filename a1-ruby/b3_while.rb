fa2ae7976b2396ebd99741b4a8b13115d7ad084a
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-26 12:08:37 +0800

    updated while

--- a1-ruby/b3_while.rb
+++ a1-ruby/b3_while.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 arr = ['What did you eat?', 'How are you?', 'Where have you been?', 'When is your birthday?', 'Are you having fun?']
-
 entry = ''
 
 while entry != 'STOP'
