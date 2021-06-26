2ef626d25f0e4e4c2b4c0a0e684dd8658736590a
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-26 12:09:02 +0800

    added object

---
+++ a2-ruby/a1_object.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Aircraft
+class Aircraft
+  def initialize(airline, manufacturer, model)
+    @airline = airline
+    @manufacturer = manufacturer
+    @model = model
+  end
+
+  def land
+    puts "#{@airline} #{@manufacturer} #{@model}, you now have permission to land."
+  end
+end
+
+pal = Aircraft.new('Philippine Airlines', 'Boeing', 'PAL-2022')
+pal.land
