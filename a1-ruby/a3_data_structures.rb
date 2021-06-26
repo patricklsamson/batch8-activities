0e01dec5d6ea801a7d5f304af53ff30131ef5a29
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-26 12:08:20 +0800

    updated data structures

--- a1-ruby/a3_data_structures.rb
+++ a1-ruby/a3_data_structures.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 # A3
-
 contact_data = [['ana@email.com', '123 Main st.', '555-123-4567'],
-
                 ['avion@email.com', '404 Not Found Dr.', '123-234-3454']]
 
 contacts = { 'Analyn Cajocson' => {}, 'Avion School' => {} }
@@ -12,13 +10,9 @@ i = 0
 
 contacts.each do |key, _value|
   contacts[key] = {
-
     email: contact_data[i][0],
-
     address: contact_data[i][1],
-
     phone: contact_data[i][2]
-
   }
 
   i += 1
