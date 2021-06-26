dd191418668d07ce984b788c0f777a4a702af254
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-26 09:25:00 +0800

    added between

---
+++ rubyactivities/between.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# B2
+
+def num_check(num)
+  if num.negative?
+
+    puts 'Number cannot be negative.'
+
+  else
+
+    case num
+
+    when 0..50
+
+      puts "#{num} is between 0 and 50."
+
+    when 51..100
+
+      puts "#{num} is between 51 and 100."
+
+    else
+
+      puts "#{num} is above 100"
+
+    end
+
+  end
+end
+
+puts 'Give a number.'
+
+num_check(gets.chomp.to_i)
