131310a8c8baeeb16b3765eaa41f7f442ab872c2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-29 19:34:17 +0800

    added count positives

---
+++ rubyactivities/count_positives.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# CODING EXERCISE 3
+input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+
+positive = input.select { |n| n > 0 }
+
+negative = input.select { |n| n < 0 }
+
+puts [].push(positive.count, negative.sum)
