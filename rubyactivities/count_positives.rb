8b112b339d5fac81d4bf9343e37136fe2815e166
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-06-29 19:51:06 +0800

    updated count positives

--- rubyactivities/count_positives.rb
+++ rubyactivities/count_positives.rb
@@ -4,9 +4,9 @@
 input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 
 if input.empty?
-  puts input
+  p input
 else
   positive = input.select { |n| n > 0 }
   negative = input.select { |n| n < 0 }
-  puts [].push(positive.count, negative.sum)
+  p [].push(positive.count, negative.sum)
 end
