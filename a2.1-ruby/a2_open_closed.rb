4d6f440a6fd09cc2d95935482bb714334f103b77
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-05 23:40:20 +0800

    added open closed

---
+++ a2.1-ruby/a2_open_closed.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# A2
+# PLAYER
+class Player
+  def initialize(name, ign)
+    @name = name
+    @ign = ign
+  end
+
+  def rank(rank)
+    puts "#{@name} with in-game name of #{@ign} has #{rank.rank}."
+  end
+end
+
+# HERALD
+class Herald < Player
+  def rank
+    '0 to 720 MMR'
+  end
+end
+
+# LEGEND
+class Legend < Player
+  def rank
+    '3360 to 4080 MMR'
+  end
+end
+
+# IMMORTAL
+class Immortal < Player
+  def rank
+    '6000+ MMR'
+  end
+end
+
+# UNRANKED
+class Unranked < Player
+  def rank
+    'no mmr'
+  end
+end
+
+player = Player.new('John Doe', 'sologamer')
+rank = Herald.new('John Doe', 'sologamer')
+player.rank(rank)
