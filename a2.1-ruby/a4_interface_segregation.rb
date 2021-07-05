2b0f4d5706224f110b05c459456512d134667d36
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-05 23:40:39 +0800

    added interface segregation

---
+++ a2.1-ruby/a4_interface_segregation.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# A4
+# ENROLLEE
+class Enrollee
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def enrollment_type(type)
+    puts "#{@first_name} #{@last_name} has enrolled for #{type.enrollment_type}."
+  end
+end
+
+# FULLPAYMENT
+class FullPayment < Enrollee
+  def enrollment_type
+    'full payment'
+  end
+end
+
+# ISA
+class ISA < Enrollee
+  def enrollment_type
+    'ISA'
+  end
+end
+
+student = Avion.new('Jane', 'Doe')
+type = ISA.new('Jane', 'Doe')
+student.enrollment_type(type)
