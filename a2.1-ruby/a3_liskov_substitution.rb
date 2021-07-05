23199ac8751cffa36c4186ff50c924b49695d33c
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-05 23:40:28 +0800

    added liskov substitution

---
+++ a2.1-ruby/a3_liskov_substitution.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# A3
+# AVION
+class Avion
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def enrollment_type(type)
+    puts "#{@first_name} #{@last_name} has enrolled for #{type.enrollment_type}."
+  end
+end
+
+# FULLPAYMENT
+class FullPayment < Avion
+  def enrollment_type
+    'full payment'
+  end
+end
+
+# ISA
+class ISA < Avion
+  def enrollment_type
+    'ISA'
+  end
+end
+
+student = Avion.new('Jane', 'Doe')
+type = FullPayment.new('Jane', 'Doe')
+student.enrollment_type(type)
