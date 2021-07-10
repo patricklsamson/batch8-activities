be3089d6ca389fd48c4499c5616322e038eced5c
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-10 17:17:54 +0800

    Update data structures

--- ba1-ruby/a3_data_structures.rb
+++ ba1-ruby/a3_data_structures.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # ACTIVITY 1 - A3
 contact_data = [['ana@email.com', '123 Main st.', '555-123-4567'],
                 ['avion@email.com', '404 Not Found Dr.', '123-234-3454']]
